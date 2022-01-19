e5320b0c2b74dcc34235f2e51262aa0d0e13c705
bwz <549068446@qq.com>
2022-01-19 16:44:55 +0800

    add root

--- run-docker.sh
+++ run-docker.sh
@@ -4,9 +4,14 @@ docker run --gpus all -it \
 --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
 -u $(id -u) \
 -v ${PWD}:${WORK_DIR} \
+-v /home/ecnu-lzw/bwz/ocr-gy:/home/ecnu-lzw/bwz/ocr-gy \
 -w ${WORK_DIR} \
 --name yolox \
 --ipc=host \
 -e QT_X11_NO_MITSHM=1 \
 -e PYTHONPATH=${WORK_DIR} \
+--user root \
 yolox
+
+# cd /home/ecnu-lzw/bwz/ocr-gy/yolox-dockerfile
+# ./run-docker.sh
